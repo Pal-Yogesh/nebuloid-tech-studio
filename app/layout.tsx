20b3cf8e2288584704a76168a6d535526ca63073
Yogesh Pal <yogeshpal5049@gmail.com>
2025-12-30 17:39:53 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Nebuloid Tech Studio",
   description: "Nebuloid Tech Studio is a web development company that provides web development services to businesses and individuals.",
+  icons:{
+    icon: "/logo.svg",
+  }
 };
 
 export default function RootLayout({
